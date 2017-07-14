8bb3604657566ec38881899d85672a7f0b6bbe1a
Corey Rodems <coreyrodems@gmail.com>
2017-07-14 16:57:34 -0700

    randomizer is broken?

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,9 +13,12 @@
 //*********************************************
 
 $(document).ready(function($) {
+
+	
 	var answerTxts = ["#answer-one-txt","#answer-two-txt","#answer-three-txt","#answer-four-txt"];
 	var answerBtns = ["#answer-one","#answer-two","#answer-three","#answer-four"];
 
+	//question objects
 	var qOne = {
 		question:'Stanley Kubrick and _________________ were the two writers of the screenplay for the 1968 film "2001: A Space Odyssey"?',
 		answers:["Arthur C. Clarke","Robert A. Heinlein", "L. Ron Hubbard", "Isaac Asimov"],
@@ -26,15 +29,19 @@ $(document).ready(function($) {
 		answers:["Oats Studios","Zygote Studios", "Rakka Limited", "Chappie Unchained"],
 		correctAnswer:"Oats Studios"
 	}
-	var qThree = {
+	var qThr = {
 		question:'The 2014 Sci Fi adventure "Edge of Tomorrow" was influced by the 2004 light novel _________________.',
 		answers:["Live, Die, Repeat","All You Need Is Kill", "Love, Live, Kill", "Killing in the Name of: Tomorrow"],
 		correctAnswer:"All You Need Is Kill"
 	}
-
-	var qsArray = [qOne, qTwo, qThree];
+	var thisVal;
+	var qsArray = [qOne, qTwo, qThr];
+	var q1;
+	var qPos = 0; 
 	
-	console.log(qOne.answers);
+	
+
+	//for the animate.css library
 	$.fn.extend({
 	    animateCss: function (animationName) {
 	        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -44,89 +51,119 @@ $(document).ready(function($) {
 	        return this;
     	}
 	});
+
+	//shuffles arrays
 	function shuffle(array) {
 	  var currentIndex = array.length, temporaryValue, randomIndex;
-
-	  // While there remain elements to shuffle...
 	  while (0 !== currentIndex) {
-
-	    // Pick a remaining element...
 	    randomIndex = Math.floor(Math.random() * currentIndex);
 	    currentIndex -= 1;
-
-	    // And swap it with the current element.
 	    temporaryValue = array[currentIndex];
 	    array[currentIndex] = array[randomIndex];
 	    array[randomIndex] = temporaryValue;
 	  }
-
 	  return array;
 	}
 
 
 
-	
+	//when page loads effects
 	$("#mainGameBox, #hud").hide();
 	$("#startYes").on("click", function(){
 		$("#startBox").addClass("animated bounceOut");
 		setTimeout(wait, 800);
 		function wait(){
+			//shuffles questions array
 			shuffle(qsArray);
+			//starts main game loop
 			mainGame();
 		}
 	});
 
+	//main game loop
 	function mainGame(){
-		var q1 = qsArray[0]; 
-		console.log(qsArray);
-		shuffle(qsArray);
-		$("#questionText").html(q1.question);
-		$("#startBox, #questionTextDiv, #timerDiv, .answers, #hintDiv").hide();
-		$("#mainGameBox").show().animateCss("bounceIn");
-		$("#hud").show().animateCss("fadeInUpBig");
-		setTimeout(wait, 1000);
-		function wait(){
-			shuffle(q1.answers);
-			$("#questionTextDiv").show().animateCss("zoomIn");
-			$("#timerDiv").show().animateCss("zoomIn");
-		}
-		setTimeout(btn1, 1800);
-		function btn1(){
-			$("#answer-one").show().animateCss("bounceInLeft");
-		}
-		setTimeout(btn2, 2000);
-		function btn2(){
-			$("#answer-two").show().animateCss("bounceInRight");
-		}
-		setTimeout(btn3, 2200);
-		function btn3(){
-			$("#answer-three").show().animateCss("bounceInLeft");
-		}
-		setTimeout(btn4, 2400);
-		function btn4(){
-			$("#answer-four").show().animateCss("bounceInRight");
-		}
-		setTimeout(btn5, 2800);
-		function btn5(){
-			$("#hintDiv").show().animateCss("bounceInUp");
-		}
-		for (var i = 0; i < 4; i++) {
-			$(answerTxts[i]).html(q1.answers[i]);
-			$(answerBtns[i]).attr("value", q1.answers[i]);
-
-		};
-		$(".answers").on("click",function(){
-			var thisVal = $(this).val();
-			console.log(thisVal);
+		setTimeout(runMain,500);
+		function runMain(){
+			//qPos is the position of the index, qPos starts at 0 so the index of the array will be 0
+			console.log("Question Array index " + qPos);
 			
-			if(thisVal === q1.correctAnswer){
-				console.log("YEAS");
+			// q1 is the question at index qPos of the array, default qPos=0
+			q1 = qsArray[qPos];
+			console.log("current question" + qsArray[qPos].correctAnswer);
+
+			//shuffles the answers array for the buttons.
+			shuffle(q1.answers);
+
+			//********************DOM animations********************
+			$("#questionText").html(q1.question);
+			$("#startBox, #questionTextDiv, #timerDiv, .answers, #hintDiv").hide();
+			$("#mainGameBox").show().animateCss("bounceIn");
+			$("#hud").show().animateCss("fadeInUpBig");
+			
+			setTimeout(wait, 1000);
+			function wait(){
+				
+				$("#questionTextDiv").show().animateCss("zoomIn");
+				$("#timerDiv").show().animateCss("zoomIn");
 			}
-			else{
-				console.log("NARP");
+			setTimeout(btn1, 1800);
+			function btn1(){
+				$("#answer-one").show().animateCss("bounceInLeft");
 			}
-		})
-		console.log(q1.answers);
+			setTimeout(btn2, 2000);
+			function btn2(){
+				$("#answer-two").show().animateCss("bounceInRight");
+			}
+			setTimeout(btn3, 2200);
+			function btn3(){
+				$("#answer-three").show().animateCss("bounceInLeft");
+			}
+			setTimeout(btn4, 2400);
+			function btn4(){
+				$("#answer-four").show().animateCss("bounceInRight");
+			}
+			setTimeout(btn5, 2800);
+			function btn5(){
+				$("#hintDiv").show().animateCss("bounceInUp");
+			}
+			//**************************************************
+
+			//adds the answers to the buttons and gives them a value for the answers
+			for (var i = 0; i < 4; i++) {
+				$(answerTxts[i]).html(q1.answers[i]);
+				$(answerBtns[i]).attr("value", q1.answers[i]);
+			};
+
+			//checks the value of the clicked button vs. the property of the object "correct answer"
+			$(".answers").on("click",function(){
+				thisVal = $(this).val();
+				console.log(thisVal);
+				debugger;
+				if(thisVal == q1.correctAnswer){
+					//Right Answer
+					console.log("YEAS");
+					setTimeout(correctA, 100);
+					function correctA(){
+
+						//on the second question it brings up, right here it is running twice. it runs through the 3 lines
+						//normally it runs once.
+						//it adds 1 to qPos
+						//it logs the qPos,
+						//then it goes to main game
+						//the index of the question array is now increased by 1, so it goes to the next question.
+						//however when this runs again, it runs twice.
+						
+						qPos += 1;
+						console.log("Question Array index " + qPos);
+						mainGame();
+					}
+				}
+				else{
+					//Wrong Answer
+					console.log("NARP");
+				}
+			});
+		}	// console.log(q1.answers);
 	}
 
 
