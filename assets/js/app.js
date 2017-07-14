3b517e85658f7886e3b5467c13579871ba8ec6a3
Corey Rodems <coreyrodems@gmail.com>
2017-07-14 14:20:56 -0700

    shuffle answers, put answers into buttons, check buttons for right answer

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,75 @@
 //*********************************************
 
 $(document).ready(function($) {
+	var answerTxts = ["#answer-one-txt","#answer-two-txt","#answer-three-txt","#answer-four-txt"];
+	var answerBtns = ["#answer-one","#answer-two","#answer-three","#answer-four"];
+	var qOne = {
+		question:'Stanley Kubrick and _________________ were the two writers of the screenplay for the 1968 film "2001: A Space Odyssey"?',
+		answers:["Arthur C. Clarke","Robert A. Heinlein", "L. Ron Hubbard", "Isaac Asimov"],
+		correctAnswer:"Arthur C. Clarke"
+	}
+	console.log(qOne.answers);
+	$.fn.extend({
+	    animateCss: function (animationName) {
+	        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	        this.addClass('animated ' + animationName).one(animationEnd, function() {
+	            $(this).removeClass('animated ' + animationName);
+	        });
+	        return this;
+    	}
+	});
+	function shuffle(array) {
+	  var currentIndex = array.length, temporaryValue, randomIndex;
 
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
+
+	console.log("ready")
+	$("#mainGameBox, #hud").hide();
+	$("#startYes").on("click", function(){
+		$("#startBox").addClass("animated bounceOut");
+		setTimeout(wait, 800);
+		function wait(){
+			shuffle(qOne.answers);
+			mainGame();
+		}
+	});
+
+	function mainGame(){
+		$("#startBox").hide();
+		$("#mainGameBox").show().animateCss("bounceIn");
+		$("#hud").show().animateCss("fadeInUpBig");
+		for (var i = 0; i < 4; i++) {
+			$(answerTxts[i]).html(qOne.answers[i]);
+			$(answerBtns[i]).attr("value", qOne.answers[i]);
+
+		};
+		$(".answers").on("click",function(){
+			var thisVal = $(this).val();
+			console.log(thisVal);
+			
+			if(thisVal === qOne.correctAnswer){
+				console.log("YEAS");
+			}
+			else{
+				console.log("NARP");
+			}
+		})
+		console.log(qOne.answers);
+	}
 
 
 
