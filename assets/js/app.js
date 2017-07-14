bbf5bafcc78cd3015e958513cfb6b7773ccd99a8
Corey Rodems <coreyrodems@gmail.com>
2017-07-14 11:03:33 -0700

    updated html

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+//***************TriviaGame***************
+//  By Corey Rodems
+
+// To Do
+//Layout webpages
+
+//Come up with questions
+
+//Time questions/Score questions
+// if question is right, 1 point move on
+// if questions is wrong, -1 point move on
+// if out of time -1 point move on
+//*********************************************
+
+$(document).ready(function($) {
+
+
+
+
+
+	
+});
